a5e438714de3abd04a7db3d832244a6250e2b9f5
cvcvcx9 <cvcvcx9@gmail.com>
2024-06-04 14:44:53 +0900

    [init] add  git ignore and init

---
+++ src/test/java/com/cvcvcx9/AutoComplete/AutoCompleteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cvcvcx9.AutoComplete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoCompleteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
